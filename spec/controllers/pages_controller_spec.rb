5033270372d729131c45beac0b24b892294b6b7b
szpaki1 <macinek@buziaczek.pl>
2012-01-15 23:18:31 +0000

    Done

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,30 +10,9 @@ describe PagesController do
     end
   end
 
-  describe "GET 'blog'" do
-    it "should be successful" do
-      get 'blog'
-      response.should be_success
-    end
-  end
-
   describe "GET 'publication'" do
     it "should be successful" do
       get 'publication'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'pdf'" do
-    it "should be successful" do
-      get 'pdf'
       response.should be_success
     end
   end
